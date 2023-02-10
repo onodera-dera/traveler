0b4154eabdc55571c0a7980ed4492cf703f771f8
onodera-dera <derahina58@gmail.com>
2023-02-11 01:52:58 +0900

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'top' => 'homes#top'
+  get 'top' => 'homes#top'
+end
